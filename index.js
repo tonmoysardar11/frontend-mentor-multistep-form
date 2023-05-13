2353ba759a55478c3af9f7c826930fc97bd9b537
Tonmoy Sardar <tonmay10fcb@gmail.com>
2023-05-13 14:43:43 +0530

    form 2 complete

--- index.js
+++ index.js
@@ -49,6 +49,9 @@ const phoneCheck=()=>{
 const next1=()=>{
     if(nameCheck() && emailCheck() && phoneCheck()){
         document.getElementById('form1').style.display="none";
+        document.getElementById('form2').style.display="block";
+        document.getElementsByClassName("widget_no")[0].classList.remove('widget_active');
+        document.getElementsByClassName("widget_no")[1].classList.add('widget_active');
     }
     else{
         nameCheck()
@@ -67,18 +70,37 @@ const changePlan=()=>{
        document.getElementById('plan_1').innerText='$90/year',
        document.getElementById('plan_2').innerText='$120/year',
        document.getElementById('plan_3').innerText='$150/year',
-       document.getElementsByClassName('free').forEach(element => {
-        element.style.opacity='1'
-       }) 
+       document.getElementById('free1').style.display='block',
+       document.getElementById('free2').style.display='block',
+       document.getElementById('free3').style.display='block'
+
        ):(
            document.getElementById('switchBtn_1').classList.add('active_switch'),
            document.getElementById('switchBtn_2').classList.remove('active_switch'),
            document.getElementById('plan_1').innerText='$9/month',
            document.getElementById('plan_2').innerText='$12/month',
            document.getElementById('plan_3').innerText='$15/month',
-           document.getElementsByClassName('free').forEach(element => {
-            element.style.opacity='0'
-           }) 
+           document.getElementById('free1').style.display='none',
+       document.getElementById('free2').style.display='none',
+       document.getElementById('free3').style.display='none'
+
        )
 }
 
+const back1=()=>{
+    document.getElementById('form1').style.display="block";
+        document.getElementById('form2').style.display="none";
+        document.getElementsByClassName("widget_no")[0].classList.add('widget_active');
+        document.getElementsByClassName("widget_no")[1].classList.remove('widget_active');
+}
+const next2=()=>{
+    if(document.getElementById('radio1').checked||document.getElementById('radio2').checked||document.getElementById('radio3').checked){
+        document.getElementById('form2').style.display="none";
+        document.getElementById('form3').style.display="block";
+        document.getElementsByClassName("widget_no")[1].classList.remove('widget_active');
+        document.getElementsByClassName("widget_no")[2].classList.add('widget_active');
+    }
+    else{
+        document.getElementById('.err2').style.display='block';
+    }
+}
